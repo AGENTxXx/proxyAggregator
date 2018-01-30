24f02831df8f06036912de0e944ef7c713c30e32
Alexander Ivanov <ivanov4291@gmail.com>
2018-01-30 16:45:19 +0700

    Init Spring Boot + Google App Engine

---
+++ src/main/java/com/enuvid/proxyaggregator/ProxyAggregatorApplication.java
@@ -0,0 +1,12 @@
+package com.enuvid.proxyaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxyAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyAggregatorApplication.class, args);
+	}
+}
---
+++ src/main/java/com/enuvid/proxyaggregator/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.enuvid.proxyaggregator;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProxyAggregatorApplication.class);
+	}
+
+}
