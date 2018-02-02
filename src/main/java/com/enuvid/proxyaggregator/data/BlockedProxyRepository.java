12382ec2e12a46ea3249e670f012784d7739c749
Alexander Ivanov <ivanov4291@gmail.com>
2018-02-03 02:54:58 +0700

    Add blocked proxies data bind

--- src/main/java/com/enuvid/proxyaggregator/data/BlockedProxyRepository.java
+++ src/main/java/com/enuvid/proxyaggregator/data/BlockedProxyRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 public interface BlockedProxyRepository extends MongoRepository<BlockedProxy, String> {
     BlockedProxy findBlockedProxyByIpAndPort(Long ip, int port);
 
-    default BlockedProxy findBlockedProxyByIpAndPort(String ip, int port) {
+    default BlockedProxy findBlockedProxy(String ip, int port) {
         return findBlockedProxyByIpAndPort(IpUtils.convert(ip), port);
     }
 }
