a3731118793a9bcbbbe102ca1e913f68c0720698
Alexander Ivanov <ivanov4291@gmail.com>
2018-02-05 15:53:21 +0700

    Fix add new proxy test

--- src/test/java/com/enuvid/proxyaggregator/data/ProxyTests.java
+++ src/test/java/com/enuvid/proxyaggregator/data/ProxyTests.java
@@ -3,10 +3,15 @@ package com.enuvid.proxyaggregator.data;
 import com.google.gson.Gson;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public class ProxyTests {
     @Test
     public void create() {
-        Proxy someProxy = new Proxy("185.136.177.192", 10, ProxyType.HTTP);
+        Proxy someProxy = new Proxy("185.136.177.192", 10, new ArrayList<ProxyType>() {{
+            add(ProxyType.HTTP);
+        }});
+
 
         System.out.println(new Gson().toJson(someProxy));
     }
