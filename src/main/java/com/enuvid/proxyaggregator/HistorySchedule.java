7cd7287af8835fb79fe2365249391797aa0f90c9
Alexander Ivanov <ivanov4291@gmail.com>
2018-02-25 13:05:40 +0700

    FileBlocked flag

--- src/main/java/com/enuvid/proxyaggregator/HistorySchedule.java
+++ src/main/java/com/enuvid/proxyaggregator/HistorySchedule.java
@@ -18,6 +18,7 @@ public class HistorySchedule {
     private final ProxyRepository proxyRepo; //Data repos
     private final BlockedProxyRepository blockedRepo;
     private Logger logger = Logger.getLogger(this.getClass().getName()); //Logger for current classname
+    private volatile boolean fileBlcoked = false;
 
     @Autowired
     public HistorySchedule(ProxyRepository proxyRepo, BlockedProxyRepository blockedRepo) {
@@ -31,6 +32,10 @@ public class HistorySchedule {
     )
     void addNextHourlyValue() {
         logger.log(Level.INFO, "Add new records to 'hourlyMetrics'");
+        while (true) {
+            if (!(fileBlcoked)) break;
+        }
+        fileBlcoked = true;
 
         DB db = DBMaker.fileDB("history.db").make();
         BTreeMap proxiesHourlyCounter = db.treeMap("proxyHourlyCounter").createOrOpen();
@@ -38,6 +43,7 @@ public class HistorySchedule {
         addRecord(proxiesHourlyCounter, (int) proxyRepo.count(), 24);
         addRecord(blockedHourlyCounter, (int) blockedRepo.count(), 24);
         db.close();
+        fileBlcoked = false;
     }
 
     @Scheduled(
@@ -46,11 +52,17 @@ public class HistorySchedule {
     )
     void addNextDailyValue() {
         logger.log(Level.INFO, "Add new records to 'dailyMetrics'");
+        while (true) {
+            if (!(fileBlcoked)) break;
+        }
+        fileBlcoked = true;
 
         DB db = DBMaker.fileDB("history.db").make();
         BTreeMap proxiesDailyCounter = db.treeMap("proxyDailyCounter").createOrOpen();
         addRecord(proxiesDailyCounter, (int) proxyRepo.count(), 30);
         db.close();
+
+        fileBlcoked = false;
     }
 
     private void addRecord(BTreeMap map, int pushValue, int maxRecords) {
